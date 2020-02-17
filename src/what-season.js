b245d8f678b78850b839c0f89e4802cdaea65c50
Evgeny <evgeny.vorokhobko@gmail.com>
2020-02-17 17:52:49 +0300

    feat: add logic in 6 classes

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,29 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  const arrays = new Map([
+    [0, 'winter'],
+    [1, 'winter'],
+    [2, 'spring'],
+    [3, 'spring'],
+    [4, 'spring'],
+    [5, 'summer'],
+    [6, 'summer'],
+    [7, 'summer'],
+    [8, 'autumn'],
+    [9, 'autumn'],
+    [10, 'autumn'],
+    [11, 'winter'],
+  ]);
+
+  if(date === undefined) {
+    return ('Unable to determine the time of year!');
+  }
+
+  let month = date.getUTCMonth();
+
+    for (var key of arrays.keys()) {
+      if (month === key) {
+        return arrays.get(month);
+        break;
+      }
+    }
 };
